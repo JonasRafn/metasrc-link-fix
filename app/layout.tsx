788fd37eaa9ac3bb00032d20f5e2a22af6aa2486
Jonas <jonaschrafn@gmail.com>
2026-08-02 17:20:37 +0200

    Add tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { PostHogProvider } from "./providers";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,7 +32,10 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<PostHogProvider>{children}</PostHogProvider>
+				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
 }
+
